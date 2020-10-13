037efd7310c73b38f9bcaabbf705a23ff37c0683
whyme0 <sparkjetstudiost@gmail.com>
2020-10-13 21:58:08 +0300

    url pattern for profile detail view added

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,27 +4,36 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
+    # Part for login, registration, passwords recovery, etc. views
     path(
-        'login/',
+        'auth/login/',
         views.UserLoginView.as_view(),
         name='login',
     ),
     
     path(
-        'registration/',
+        'auth/registration/',
         views.UserRegistrationView.as_view(),
         name='registration',
     ),
 
     path(
-        'emailverification/<slug:token>',
+        'auth/emailverification/<slug:token>',
         views.EmailConfirmationView.as_view(),
         name='email_confirmation',
     ),
 
     path(
-        'resend-confirmation-email/',
+        'auth/resend-confirmation-email/',
         views.ResendEmailConfirmation.as_view(),
         name='resend_confirmation_email',
     ),
+
+
+    # Part specially for user, user settings, user data, etc.
+    path(
+        'profile/<slug:username>',
+        views.ProfileView.as_view(),
+        name='profile',
+    )
 ]
