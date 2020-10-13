9d2c43beaec3ab5efce115135464d334f8968186
whyme0 <sparkjetstudiost@gmail.com>
2020-10-13 23:24:57 +0300

    created file with root api urlpatterns

---
+++ api/api_urls.py
@@ -0,0 +1,9 @@
+#----------------------------------------------
+#
+# File with all api's urlspatterns in one place
+#
+#----------------------------------------------
+from api.users.urls import urlpatterns as users_urlpattern
+
+urlpatterns = []
+urlpatterns += users_urlpattern
