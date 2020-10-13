c3762f8ceb12e27c7038460f371ed7c6e931f6a3
whyme0 <sparkjetstudiost@gmail.com>
2020-10-13 18:41:20 +0300

    serializer for profile model created

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from apps.users.models import Profile
 
 
-class UserSerializer(serializers.Sea):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['id', 'username', 'avatar_image', 'date_joined']
