fc805b369cd952e9aa578e62b6f896b41c020737
whyme0 <sparkjetstudiost@gmail.com>
2020-10-15 18:33:49 +0300

    new models registred

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, EmailVerification, PasswordRecovery
 
 admin.site.register(Profile)
+admin.site.register(EmailVerification)
+admin.site.register(PasswordRecovery)
