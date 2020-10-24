3491b04642a8f2cbddc4b9937e27c36171769883
whyme0 <sparkjetstudiost@gmail.com>
2020-10-24 09:53:14 +0300

    created new test for PasswordResetForm

--- apps/users/tests/test_forms.py
+++ apps/users/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from ..forms import AskEmailForm
+from ..forms import AskEmailForm, PasswordResetForm
 from ..models import Profile
 
 
@@ -30,3 +30,60 @@ class TestAskEmailForm(TestCase):
         """
         f = AskEmailForm(data={'email': 'tempmail@mail.co'})
         self.assertTrue(f.is_valid())
+
+
+class TestPasswordResetForm(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.u = Profile.objects.create_user(
+            username='temp1',
+            email='temp1@mail.co',
+            password='hardpwd123',
+        )
+    
+    def test_form_validation_for_errors(self):
+        f1 = PasswordResetForm(
+            user=self.u,
+            data={
+                'new_password1': '123',
+                'new_password2': '123',
+            }
+        )
+
+        errs = list(f1.errors.values())[0]
+
+        self.assertFalse(f1.is_valid())
+
+        self.assertIn('This password is too short. It must contain at least 8 characters.', errs)
+        self.assertIn('This password is too common.', errs)
+        self.assertIn('This password is entirely numeric.', errs)
+
+        f2 = PasswordResetForm(
+            user=self.u,
+            data={
+                'new_password1': 'uiuiuiui',
+                'new_password2': 'iuiuiuiu',
+            }
+        )
+
+        errs = list(f2.errors.values())[0]
+        self.assertFalse(f2.is_valid())
+
+        self.assertIn('The two password fields didn’t match.', errs)
+
+    def test_form_validation_for_success(self):
+        self.assertTrue(self.u.check_password('hardpwd123'))
+
+        f1 = PasswordResetForm(
+            user=self.u,
+            data={
+                'new_password1': 'goodPwd345',
+                'new_password2': 'goodPwd345',
+            }
+        )
+
+        self.assertTrue(f1.is_valid())
+        f1.save()
+
+        self.assertFalse(self.u.check_password('hardpwd123'))
+        self.assertTrue(self.u.check_password('goodPwd345'))
