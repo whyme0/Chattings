036d824b59dd241d3a53e35c69b3cb393e6a9bbc
whyme0 <sparkjetstudiost@gmail.com>
2020-10-26 16:06:42 +0300

    commit

--- api/api_urls.py
+++ api/api_urls.py
@@ -1,7 +1,5 @@
 #----------------------------------------------
-#
 # File with all api's urlspatterns in one place
-#
 #----------------------------------------------
 from api.users.urls import urlpatterns as users_urlpattern
 
