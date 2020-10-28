74e9fbe53b77401ae735217552e919654043f5a3
whyme0 <sparkjetstudiost@gmail.com>
2020-10-28 22:44:32 +0300

    modified scripts for profile details template

--- static/users/profiles/profile_view/profile_details.js
+++ static/users/profiles/profile_view/profile_details.js
@@ -1,2 +1,28 @@
-const profileInfoOpenButton = document.getElementById("profileInfoOpenButton");
-const profileInfoCloseButton = document.getElementById("profileInfoCloseButton");
+let profileInfoOpenButton = document.getElementById("profileInfoOpenButton");
+let profileChannelsOpenButton = document.getElementById("profileChannelsOpenButton");
+
+let dropdownProfileInfo = document.querySelector(".dropdown__profile-info");
+let dropdownProfileChannels = document.querySelector(".dropdown__profile-channels");
+
+profileInfoOpenButton.onclick = function(){
+    if (dropdownProfileInfo.style.display === "none"){
+        profileInfoOpenButton.innerText = "Close profile info";
+        dropdownProfileInfo.style.display = "block";
+    }
+    else {
+        profileInfoOpenButton.innerText = "Show profile info";
+        dropdownProfileInfo.style.display = "none";
+    }
+}
+
+
+profileChannelsOpenButton.onclick = function(){
+    if (dropdownProfileChannels.style.display === "none"){
+        profileChannelsOpenButton.innerText = "Close profile channels";
+        dropdownProfileChannels.style.display = "block";
+    }
+    else {
+        profileChannelsOpenButton.innerText = "Show profile channels";
+        dropdownProfileChannels.style.display = "none";
+    }
+}
