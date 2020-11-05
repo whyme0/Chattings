4d9f9e9bc6ea144ac0232f1a3f71743a6ef076cf
whyme0 <sparkjetstudiost@gmail.com>
2020-11-05 21:52:00 +0300

    changes

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,12 +1,19 @@
+from typing import Dict, List
+
 from rest_framework import serializers
 
 from apps.users.models import Profile
 
 
+META_FIELDS: Dict[str, List[str]] = {
+    'PROFILE_SERIALIZER_FIELDS': ['url', 'id', 'avatar_image'],
+}
+
+
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
-        fields = ['url', 'id', 'username', 'email', 'avatar_image', 'date_joined']
+        fields = META_FIELDS['PROFILE_SERIALIZER_FIELDS']
         extra_kwargs = {
             'url': {'view_name': 'users:profile', 'lookup_field': 'pk'}
         }
