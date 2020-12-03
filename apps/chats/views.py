ba7ea4c75f31e0d8dcfb0c95bc08e39105bfc8b8
whyme0 <sparkjetstudiost@gmail.com>
2020-12-03 17:36:25 +0300

    View for displayng ChatsList created

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -1,3 +1,11 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Chat
+
+
+class ChatsList(ListView):
+    model = Chat
+    paginate_by = 30
+    context_object_name = 'chats'
+    template_name = 'chats/chats_list/template.html'
