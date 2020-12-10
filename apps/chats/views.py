6eb603a51ed5debf14b5573d6341f8271b96d552
whyme0 <sparkjetstudiost@gmail.com>
2020-12-10 23:02:50 +0300

    created new view for creation chat

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView
 from django.views.generic import ListView
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Chat
 
@@ -9,4 +14,20 @@ class ChatsList(ListView):
     paginate_by = 16
     ordering = '?'
     context_object_name = 'chats'
-    template_name = 'chats/chats_list/template.html'
+    template_name = 'chats/chats_list/chats_list.html'
+
+
+@method_decorator(login_required(redirect_field_name=None), name='dispatch')
+@method_decorator(never_cache, name='dispatch')
+class ChatCreateView(CreateView):
+    model = Chat
+    template_name = 'chats/create_chat/create_chat.html'
+    fields = ['label', 'description', 'name', 'avatar']
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('chats:chat-list')
+
