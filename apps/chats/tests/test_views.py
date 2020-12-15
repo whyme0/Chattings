8e7061d5dc2e1a016d35790d320fb1f50388cf23
whyme0 <sparkjetstudiost@gmail.com>
2020-12-15 16:19:16 +0300

    created file for with test for chats app's views

---
+++ apps/chats/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from ..views import ChatsList
+from ..models import Chat
+from users.models import Profile
+
+
+class TestChatsListView(TestCase):
+    pass
