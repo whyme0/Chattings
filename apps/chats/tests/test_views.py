b70d217be0c72677549db0a29a3e36fcc2ee6497
whyme0 <sparkjetstudiost@gmail.com>
2020-12-15 16:23:36 +0300

    new test added

--- apps/chats/tests/test_views.py
+++ apps/chats/tests/test_views.py
@@ -1,10 +1,34 @@
 from django.test import TestCase
 from django.urls import reverse
+from bs4 import BeautifulSoup
 
 from ..views import ChatsList
 from ..models import Chat
-from users.models import Profile
+from apps.users.models import Profile
 
 
 class TestChatsListView(TestCase):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.u1 = Profile.objects.create_user(
+            username='testuser',
+            email='testuser@mail.com',
+            password='hardpwd123'
+        )
+
+        for i in range(1, 11):
+            chat1 = Chat.objects.create(
+                owner=cls.u1,
+                label=f'Label №{i}',
+                name=f'name_{i}',
+            )
+
+    def test_basics(self):
+        response = self.client.get(
+            reverse('chats:chat-list'),
+        )
+
+        page_title = BeautifulSoup(response.content, 'html.parser').find('title').getText().strip().replace('\n', '')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(page_title, 'Chats list \ Chattings')
+        self.assertEqual(response.resolver_match.func.view_class, ChatsList)
