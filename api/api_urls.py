0cd11979a48ba09f76aa86ba8320c29781939fc1
whyme0 <sparkjetstudiost@gmail.com>
2020-12-15 20:11:07 +0300

    patterns for chats added to root

--- api/api_urls.py
+++ api/api_urls.py
@@ -1,7 +1,9 @@
 #----------------------------------------------
 # File with all api's urlspatterns in one place
 #----------------------------------------------
-from api.users.urls import urlpatterns as users_urlpattern
+from api.users.urls import urlpatterns as users_urlpatterns
+from api.chats.urls import urlpatterns as chats_urlpatterns
 
 urlpatterns = []
-urlpatterns += users_urlpattern
+urlpatterns += users_urlpatterns
+urlpatterns += chats_urlpatterns
