19e2bdea9feca1fea134017c22b0c429ace6b88a
whyme0 <sparkjetstudiost@gmail.com>
2020-12-18 22:07:22 +0300

    created new view

--- api/chats/views.py
+++ api/chats/views.py
@@ -1,3 +1,5 @@
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 from .serializers import ChatSerializer
@@ -7,3 +9,11 @@ from apps.chats.models import Chat
 class ChatViewSet(viewsets.ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+
+
+class ChatMembersView(RetrieveAPIView):
+    queryset = Chat.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        chat = self.get_object()
+        return Response({'members': chat.members})
