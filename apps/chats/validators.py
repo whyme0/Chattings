19ae76bd61ca62f78ac612ebae813a5caa594a24
whyme0 <sparkjetstudiost@gmail.com>
2021-01-06 14:01:17 +0300

    new validator for files created

--- apps/chats/validators.py
+++ apps/chats/validators.py
@@ -4,3 +4,8 @@ from django.core.exceptions import ValidationError
 def validate_empty_string(val):
     if len(val.split()) == 0:
         raise ValidationError("Field is empty.")
+
+def file_size_validator(self):
+    # If size > 10Mb
+    if img.file.size > 1024 * 1024 * 10:
+        raise ValidationError('This file too large.')
