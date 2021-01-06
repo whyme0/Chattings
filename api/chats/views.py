a2018a21ba3cc769c8931e8aeb230023e8b7852a
whyme0 <sparkjetstudiost@gmail.com>
2021-01-06 13:59:12 +0300

    ChatViewSet improved

--- api/chats/views.py
+++ api/chats/views.py
@@ -1,14 +1,20 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework import viewsets
 
+from .permissions import IsOwnerOrAuthenticatedOrReadOnly
 from .serializers import ChatSerializer
 from apps.chats.models import Chat
 
 
 class ChatViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrAuthenticatedOrReadOnly]
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class ChatMembersView(RetrieveAPIView):
