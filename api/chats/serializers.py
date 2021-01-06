5b778623c6802c7627429d0bfe16303b2dd998a1
whyme0 <sparkjetstudiost@gmail.com>
2021-01-06 13:58:04 +0300

    name field become read-onlu

--- api/chats/serializers.py
+++ api/chats/serializers.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 
 from apps.chats.models import Chat
@@ -13,6 +14,16 @@ META_FIELDS: Dict[str, List[str]] = {
 
 
 class ChatSerializer(serializers.HyperlinkedModelSerializer):
+    name = serializers.SlugField(
+        max_length=50,
+        help_text='Name must be unique and cannot be changed.',
+    )
+
+    def validate_name(self, value):
+        if self.instance and self.instance.name != value:
+            raise ValidationError('You can\'t edit this field')
+        return value
+
     class Meta:
         model = Chat
         fields = META_FIELDS['CHAT_SERIALIZER_FIELDS']
