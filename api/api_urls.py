697aedefd307a45ceaacf9424559d141d0a0f1c3
whyme0 <sparkjetstudiost@gmail.com>
2021-01-06 13:56:49 +0300

    added urlpatterm for authorization by token

--- api/api_urls.py
+++ api/api_urls.py
@@ -1,9 +1,14 @@
 #----------------------------------------------
 # File with all api's urlspatterns in one place
 #----------------------------------------------
+from django.urls import path
+from rest_framework.authtoken import views
+
 from api.users.urls import urlpatterns as users_urlpatterns
 from api.chats.urls import urlpatterns as chats_urlpatterns
 
-urlpatterns = []
+urlpatterns = [
+    path('token-auth/', views.obtain_auth_token, name='api-token-auth'),
+]
 urlpatterns += users_urlpatterns
 urlpatterns += chats_urlpatterns
